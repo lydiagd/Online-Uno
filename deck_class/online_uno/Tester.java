3e503ab6678ac7c52247e04f15a469a5a989d509
Lydia DiBlasio <diblasio@usc.edu>
2020-10-27 11:50:16 -0700

    Update Tester.java

--- deck_class/online_uno/Tester.java
+++ deck_class/online_uno/Tester.java
@@ -5,11 +5,14 @@ public class Tester {
 	public static void main(String [] args) 
 	{
 		Deck d = new Deck();
+		System.out.println("Card deck contents:");
 		for(Card c : d.cards)
 		{
 			System.out.print(c.stringout() + " ");
 		}
+		System.out.println();
 		
+		System.out.println("Shuffled card deck:");
 		d.shuffle();
 		for(Card c : d.cards)
 		{
