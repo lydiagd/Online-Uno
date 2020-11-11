600759542de83aeae27db6d82c2f230fac20004e
Chloe Kuo <cmkuo@usc.edu>
2020-11-11 12:54:13 -0800

    Update

--- server/PlayerClient.java
+++ server/PlayerClient.java
@@ -74,7 +74,7 @@ public static void main(String[] args){
             	 System.out.print("your hand: ");
             	 for(String str : hand)
             	 {
-            		 System.out.print(" | " + s + " | ");
+            		 System.out.print(" | " + str + " | ");
             	 }
               
             	 break;
