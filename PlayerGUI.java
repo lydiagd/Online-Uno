b28598ee9d8ac6e095aa683cd9942314f3cbbaa1
PatrickJarvis <pjarvis@usc.edu>
2020-11-11 13:04:54 -0800

    Updated for better access with Player class

--- PlayerGUI.java
+++ PlayerGUI.java
@@ -1,6 +1,4 @@
-package online-uno;
-
-//Author: Patrick Jarvis
+package project;
 
 import java.awt.Font;
 import java.awt.Image;
@@ -14,7 +12,25 @@ import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+//
+//
+//TODO:
+//Only allow for Player to modify Player GUI
+//Add Wildcard Card
+//Only play viable moves
+//
+//
+//
+//
+//
+//
+//
+//
+
+import GameTable.*;
+
 public class PlayerGUI{
+	public String moveMade = "None";
 	//handles the dimensions of the pop-up
 	private JFrame frame;
 	private JPanel panel;
@@ -35,15 +51,21 @@ public class PlayerGUI{
 	private JLabel blank;
 	private JButton back;
 	
+	private JLabel wildcard;
+	private JComboBox colors;
+	private JButton select;
+	
 	//Button to play a specific card
 	private JButton play;
 	//button to see the other cards in your hand
 	private JButton nextPage;
 	//Drop down box for player to pick which card to play
 	private JComboBox hand;
+	//TODO: Wildcard drop down box
 	
 	private int page = 0;
-	private String[] playerHand;
+	private Card[] playerHand;
+	private String[] names;
 	private boolean canPlay = false;
 	
 	//images of the cards in your hand
@@ -55,24 +77,46 @@ public class PlayerGUI{
 	private JLabel hand5;
 	private JLabel hand6;
 	
+	//in the future, GUI should also take in a Player class so that it can tell the back-end whenever a move is made
 	//Constructor of the GUI,
-	public PlayerGUI(String[] handIn) throws IOException {
+	public PlayerGUI(Card[] handIn, String username) throws IOException {
 		//create the framework for the application
 		this.playerHand = handIn;
 		frame = new JFrame();
 		panel = new JPanel();
 		panel.setLayout(null);
 		
-		pages = new JLabel("Page " + (page+1) + " of " + (playerHand.length/7 + 1));
+		pages = new JLabel("Page " + (page+1) + " of " + ((playerHand.length-1)/7 + 1));
 		
 		play = new JButton("Play");
 		nextPage = new JButton("Next Page");
+		
+		
+		wildcard = new JLabel("What color?");
+		String[] unoColors = {"Yellow", "Red", "Green", "Blue"};
+		colors = new JComboBox(unoColors);
+		select = new JButton("Select");
 
+		select.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//only play a card if their hand isn't empty
+				String color = (String) colors.getSelectedItem();
+				String answer = "Wildcard " + color;
+				setFaceUp(answer);
+				removeCard("Wildcard");
+				moveMade = answer;
+				select.setVisible(false);
+				colors.setVisible(false);
+				wildcard.setVisible(false);
+			}
+		});
+		
+		
 		//sets up the image of where the face up cards are going to be placed
 		BufferedImage blankIm = ImageIO.read(new File("CardImages/blank.PNG"));
 		Image resizeBlank = blankIm.getScaledInstance(100, -1, 0);
 		blank = new JLabel(new ImageIcon(resizeBlank));
-		blank.setBounds(355, 140, 100, 200);
+		blank.setBounds(355, 140, 110, 200);
 		panel.add(blank);
 		
 		//sets up the image of where the face down deck is going to be placed
@@ -114,7 +158,11 @@ public class PlayerGUI{
 		displayHand();
 		
 		//create drop down menu based on the array of cards provided
-		hand = new JComboBox(playerHand);
+		names = new String[playerHand.length];
+		for(int i = 0; i < playerHand.length; i++) {
+			names[i] = playerHand[i].stringout();
+		}
+		hand = new JComboBox(names);
 		
 		//TODO: implement function that determines if the card selected is a valid play
 		//when the player clicks the Play button, the card selected by the drop down is played
@@ -122,14 +170,23 @@ public class PlayerGUI{
 			public void actionPerformed(ActionEvent e) {
 				//only play a card if their hand isn't empty
 				if(canPlay) {
+					
 					if(playerHand.length > 0) {
 						//if they are able to play their last card, they win
 						if(playerHand.length==1)
 							System.out.println("YOU WIN!");
-						String answer = (String) hand.getSelectedItem();
+						String answer = (String)hand.getSelectedItem();
+						if(answer.compareTo("Wildcard") == 0) {
+							select.setVisible(true);
+							colors.setVisible(true);
+							wildcard.setVisible(true);
+						}
+						else{
 						//put the selected card in the face up pile and remove from your hand
-						setFaceUp(answer);
-						removeCard(answer);
+							setFaceUp(answer);
+							removeCard(answer);
+							moveMade = answer;
+						}
 					}
 				}
 			}
@@ -146,21 +203,22 @@ public class PlayerGUI{
 		//when you click the face down deck, you should draw a card. Hasn't been created yet so it says Play Ball because I was watching a Dodgers game
 		back.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(canPlay)
+				if(canPlay) {
 					System.out.println("Play ball");
-				
+					moveMade = "Draw";
+				}
 			}
 		});
 		
 		//TODO: create function that updates the other players' num of cards
 		//Text displaying each player in the room and the number of cards each has
-		player1 = new JLabel("You: Guest01823930412");
+		player1 = new JLabel("You: " + username);
 		cards1 = new JLabel("Cards: " + playerHand.length);
-		player2 = new JLabel("Guest01823930412");
+		player2 = new JLabel("Guest0");
 		cards2 = new JLabel("Cards: " + playerHand.length);
-		player3 = new JLabel("Guest01823930413");
+		player3 = new JLabel("Guest0");
 		cards3 = new JLabel("Cards: " + playerHand.length);
-		player4 = new JLabel("Guest01823930414");
+		player4 = new JLabel("Guest0123456789");
 		cards4 = new JLabel("Cards: " + playerHand.length);
 		
 		//display Player 1's text
@@ -177,8 +235,8 @@ public class PlayerGUI{
 		BufferedImage p2 = ImageIO.read(new File("CardImages/p2.PNG"));
 		Image resizep2 = p2.getScaledInstance(100, -1, 0);
 		player2.setIcon(new ImageIcon(resizep2));
-		player2.setIconTextGap(-120);
-		player2.setBounds(530, 100, 280, 200);
+		player2.setIconTextGap(-184 + (8*(16-player2.getText().length()/2)));
+		player2.setBounds(594-(8*player2.getText().length()/2), 100, 280, 200);
 		cards2.setBounds(560, 230, 100, 40);
 		player2.setFont(new Font(player1.getFont().getName(), Font.PLAIN, 16));
 		cards2.setFont(new Font(player1.getFont().getName(), Font.PLAIN, 16));
@@ -190,7 +248,7 @@ public class PlayerGUI{
 		Image resizep3 = p3.getScaledInstance(100, -1, 0);
 		cards3.setIcon(new ImageIcon(resizep3));
 		cards3.setIconTextGap(-87);
-		player3.setBounds(255, 0, 280, 40);
+		player3.setBounds(329-(8*player3.getText().length()/2), 0, 280, 40);
 		cards3.setBounds(280, 0, 100, 130);
 		player3.setFont(new Font(player1.getFont().getName(), Font.PLAIN, 16));
 		cards3.setFont(new Font(player1.getFont().getName(), Font.PLAIN, 16));
@@ -201,8 +259,8 @@ public class PlayerGUI{
 		BufferedImage p4 = ImageIO.read(new File("CardImages/p4.PNG"));
 		Image resizep4 = p4.getScaledInstance(100, -1, 0);
 		player4.setIcon(new ImageIcon(resizep4));
-		player4.setIconTextGap(-120);
-		player4.setBounds(20, 100, 280, 200);
+		player4.setIconTextGap(-184 + (8*(16-player4.getText().length()/2)));
+		player4.setBounds(84-(8*player4.getText().length()/2), 100, 280, 200);
 		cards4.setBounds(50, 230, 100, 40);
 		player4.setFont(new Font(player1.getFont().getName(), Font.PLAIN, 16));
 		cards4.setFont(new Font(player1.getFont().getName(), Font.PLAIN, 16));
@@ -218,6 +276,19 @@ public class PlayerGUI{
 		
 		nextPage.setBounds(580, 370, 100, 40);
 		panel.add(nextPage);
+		
+		select.setBounds(290, 340, 100, 40);
+		panel.add(select);
+		
+		colors.setBounds(160, 340, 120, 40);
+		panel.add(colors);
+		
+		wildcard.setBounds(70, 340, 100, 40);
+		panel.add(wildcard);
+		
+		select.setVisible(false);
+		colors.setVisible(false);
+		wildcard.setVisible(false);
 
 		//display the application
 		frame.add(panel);
@@ -233,30 +304,24 @@ public class PlayerGUI{
 	//The main only creates a sample hand with the GUI, this can be altered for when we implement the Player class
 	public static void main(String[] args) {
 		try {
-			ArrayList<String> cards = new ArrayList<String>();
-			cards.add("Yellow 4");
-			cards.add("Green 4");
-			cards.add("Green 9");
-			cards.add("Blue 7");
-			cards.add("Yellow 2");
-			cards.add("Red 4");
-			cards.add("Red 9");
-			cards.add("Blue 1");
-			cards.add("Blue 0");
-			cards.add("Blue 7");
-			cards.add("Yellow 2");
-			cards.add("Red 4");
-			cards.add("Yellow 7");
-			cards.add("Blue 4");
-			cards.add("Green 2");
+			ArrayList<Card> cards = new ArrayList<Card>();
+			cards.add(new Wild_Card("Wildcard"));
+			cards.add(new Normal_Card("Yellow", 4));
+			cards.add(new Normal_Card("Red", 5));
+			cards.add(new Normal_Card("Yellow", 0));
+			cards.add(new Normal_Card("Blue", 2));
+			cards.add(new Normal_Card("Blue", 1));
+			cards.add(new Normal_Card("Green", 9));
+			cards.add(new Normal_Card("Yellow", 7));
+			cards.add(new Normal_Card("Yellow", 3));
 			
-			String[] cardsH = new String[cards.size()];
+			Card[] cardsH = new Card[cards.size()];
 			int x = 0;
-			for(String i : cards) {
+			for(Card i : cards) {
 				cardsH[x++] = i;
 			}
-			PlayerGUI GUI = new PlayerGUI(cardsH);
-			GUI.changeTurns(false);
+			PlayerGUI GUI = new PlayerGUI(cardsH, "I_Hate_This");
+			GUI.changeTurns(true);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -264,14 +329,16 @@ public class PlayerGUI{
 	}
 	
 	//function that changes which card is displayed in the face up pile
-	public void setFaceUp(String newCard) {
+	//TODO: Card not String
+	public void setFaceUp(String temp) {
 		BufferedImage image;
 		try {
-			String[] card = newCard.split("\s");
-			if(card.length != 2)
-				throw new IOException();
-			image = ImageIO.read(new File("CardImages/" + card[0].toLowerCase() + card[1] + ".PNG"));
-			Image resize = image.getScaledInstance(100, -1, 0);
+			String[] card = temp.split("\s");
+			if(card.length == 1)
+				image = ImageIO.read(new File("CardImages/" + card[0] + ".PNG"));
+			else
+				image = ImageIO.read(new File("CardImages/" + card[0].toLowerCase() + card[1] + ".PNG"));
+			Image resize = image.getScaledInstance(110, -1, 0);
 			blank.setIcon(new ImageIcon(resize));
 		}
 		catch(IOException ex) {}
@@ -286,11 +353,18 @@ public class PlayerGUI{
 			BufferedImage image;
 			if(i<playerHand.length) {
 				try {
-					String newCard = playerHand[i];
-					String[] card = newCard.split("\s");
-					image = ImageIO.read(new File("CardImages/" + card[0].toLowerCase() + card[1] + ".PNG"));
+					Card newCard = playerHand[i];
+					String temp = newCard.stringout();
+					String[] card = temp.split("\s");
+					if(card.length == 1) {
+						image = ImageIO.read(new File("CardImages/" + card[0] + ".PNG"));
+					}
+					else {
+						image = ImageIO.read(new File("CardImages/" + card[0].toLowerCase() + card[1] + ".PNG"));
+					}
 					Image resize = image.getScaledInstance(60, -1, 0);
 					curr.setIcon(new ImageIcon(resize));
+					
 				}
 				catch(IOException ex) {}
 			}
@@ -306,33 +380,38 @@ public class PlayerGUI{
 	}
 	
 	//remove a card from the drop down menu and from your playerHand array
-	public void removeCard(String select) {
+	public void removeCard(String temp) {
 		hand.removeAllItems();
-		
-		String[] newHand = new String[playerHand.length-1];
+		Card[] newHand = new Card[playerHand.length - 1];
+		String[] newNames = new String[names.length - 1];
 		boolean removed = false;
 		int j = 0;
 		for(int i = 0; i< newHand.length; i++) {
-			if(select.compareTo(playerHand[j])==0) {
+			if(temp.compareTo(names[i]) == 0) {
 				if(!removed) {
 					removed = true;
 					j++;
 				}
 			}
+			newNames[i] = names[j];
 			newHand[i] = playerHand[j++];
-			hand.addItem(newHand[i]);
+			hand.addItem(newNames[i]);
 		}
 		setHand(newHand);
+		setNames(newNames);
 		cards1.setText("Cards: " + playerHand.length);
 	}
 	
 	//updates your hand variable
-	public void setHand(String[] cards) {
+	public void setHand(Card[] cards) {
 		this.playerHand = cards;
 		
 		page = 0;
 		pages.setText("Page " + (page+1) + " of " + ((playerHand.length-1)/7 + 1));
 		displayHand();
+	}
+	public void setNames(String[] n) {
+		this.names = n;
 	}
 	
 	//change the page displaying cards
@@ -373,6 +452,3 @@ public class PlayerGUI{
 	}
 }
 
-
-
-//Server gives the GUI an ArrayList that has the cards in my hand
