a035c04350b2a04bccdf4dcf3f702c0f353038ff
PatrickJarvis <pjarvis@usc.edu>
2020-11-14 17:16:15 -0800

    Merging GUI with Player
    Comments made to integrate GUI with Player class

--- GameTable/Player.java
+++ GameTable/Player.java
@@ -53,39 +53,48 @@ public class Player extends Thread {
 			//get card
 			String cardInput = (String) ois.readObject();
 			
-			if(cardInput.equals("wildcard"))
-			{
-				String newColor = (String) ois.readObject();
-				for(int i = 0; i < hand.size(); i++)
-				{
-					cur = hand.get(i);
-					if(cur.getColor().equals(cardInput))
-					{
-						cur.setColor(newColor);
-						//remove selected card from hand and put on table's discard stack
-						table.discardStk.discard(cur); //add to discard stack
-						hand.remove(i);
-						break;
-					}
-					
-				}
-			}
+//N			if(cardInput.equals("wildcard"))
+//			{
+//				String newColor = (String) ois.readObject();
+//				for(int i = 0; i < hand.size(); i++)
+//				{
+//					cur = hand.get(i);
+//					if(cur.getColor().equals(cardInput))
+//					{
+//						cur.setColor(newColor);
+//						//remove selected card from hand and put on table's discard stack
+//						table.discardStk.discard(cur); //add to discard stack
+//						hand.remove(i);
+//						break;
+//					}
+//					
+//				}
+//			}
+			//just an if statement
 			else if(cardInput.equals("draw")) //player needs to draw a new card
 			{
 				if(table.drawStk.size() == 0) //if ran out of cards, reload drawStack
 				{
 					table.ReShuffle();
 				}
-				hand.add(table.drawStk.draw()); //add the top of the draw stack to hand
+
+			hand.add(table.drawStk.draw()); //add the top of the draw stack to hand
 			}
+			
+			//including wildcard in this one
 			else { //if not a wildcard or drawing a card, find regular card in hand
 			//process card
 			//this is assuming the card is a viable move
 				String[] cardPlayedInfo = cardInput.split(" "); //assuming this is in format "blue 3"
 				//handle wildcard here
 				
-				
 				int cardNum = Integer.parseInt(cardPlayedInfo[1]);
+//Y				String newColor = cardPlayedInfo[0];
+//				if(cardNum == -1)
+//					cardPlayedInfo[0] = "wildcard";
+//				Blue -1
+				
+				
 				for(int i = 0; i < hand.size(); i++)
 				{
 					cur = hand.get(i);
@@ -93,6 +102,9 @@ public class Player extends Thread {
 					{
 						//remove selected card from hand and put on table's discard stack
 						table.discardStk.discard(cur); //add to discard stack
+						if(cardNum == -1) {
+//Y							cur.setColor(newColor);
+						}
 						hand.remove(i);
 						break;
 					}
@@ -180,4 +192,3 @@ public class Player extends Thread {
   //   }
 	
   }
-  
